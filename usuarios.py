d3fc5470077bc449b8c97e467986dad3565b6a1f
ramon-heerdt <137829996+ramon-heerdt@users.noreply.github.com>
2026-09-22 17:29:00 -0300

    `começando o def de users

--- usuarios.py
+++ usuarios.py
@@ -1,16 +1,19 @@
 import sqlite3
 
 #conectando o banco de dados. Caso não existe, o banco é criado.
-conn = sqlite3.connect("biblioteca.db")
+
+def cadastrar_usuario():
+    conn = sqlite3.connect("biblioteca.db")
 
 #Apaga a tabela usuarios
-conn.execute("DROP TABLE IF EXISTS usuarios")
+    conn.execute("DROP TABLE IF EXISTS usuarios")
 
 #cria a tab usuarios
-conn.execute("CREATE TABLE usuarios (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT NOT NULL)")
-
+    conn.execute("CREATE TABLE usuarios (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT NOT NULL)")
+    
 #Inserindo os registros na tbela usuarios
-conn.executemany("INSERT INTO usuarios(nome) VALUES(?)", [("Leopoldo II",), ("Pol Pot",), ("Mao Tsé-Tung",)])
+    a = input("Digite o nome do usuario:")
+    conn.execute("INSERT INTO usuarios(nome) VALUES(?)", [(a)])
 
 #Confirmando a criação e os inserts da tabela usuarios
-conn.commit()
+    conn.commit()
